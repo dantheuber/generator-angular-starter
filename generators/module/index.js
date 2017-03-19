5a2dab24028568d78d051c1f0512ea22b735e62c
Daniel Essig <dan.essig@gmail.com>
2017-03-19 00:46:16 -0700

    prompt for init type after confirming module doesnt exist

--- generators/module/index.js
+++ generators/module/index.js
@@ -17,48 +17,52 @@ module.exports = Generator.extend({
         name: 'moduleName',
         message: 'What would you like to name this new module?',
         default: 'newModule'
-      }, {
+      }];
+      return this.prompt(prompts);
+    }.bind(this);
+
+    var handlePrompt = function (props) {
+      if (_.includes(modules, props.moduleName)) {
+        this.log(chalk.red(props.moduleName) + ' module already exists. Choose another name.');
+        return promptForModuleName().then(handlePrompt);
+      }
+      var initPrompt = [{
         type: 'list',
         name: 'initType',
         message: 'Initialize module with which factory?',
         default: 'controller',
         choices: ['controller','service']
       }];
-      return this.prompt(prompts);
-    }.bind(this);
-
-    var handlePrompt = function (props) {
-      var newModule = props.moduleName;
-      if (_.includes(modules, newModule)) {
-        this.log(chalk.red(newModule) + ' module already exists. Choose another name.');
-        return promptForModuleName().then(handlePrompt);
-      }
-
-      // create new module
-      var initType = props.initType;
-      var templateVars = {
-        moduleName: newModule,
-        moduleNameUp: newModule.charAt(0).toUpperCase() + newModule.slice(1),
-        typeLower: initType,
-        typeUpper: initType.charAt(0).toUpperCase() + initType.slice(1)
-      };
-      var moduleDir = 'src/' + newModule;
-      var moduleFilePath = moduleDir + '/' + newModule + '.module.js';
-      fs.mkdirSync(moduleDir);
-      this.fs.copyTpl(
-        this.templatePath('module.js'),
-        this.destinationPath(moduleFilePath),
-        templateVars
-      );
-      var factDest = 'src/' + newModule + '.' + initType + '.js';
-      this.fs.copyTpl(
-        this.templatePath('factory.js'),
-        this.destinationPath(factDest),
-        templateVars
-      );
+      return this.prompt(initPrompt).then(function (initProps) {
+        this.props = _.extend(props, initProps);
+      }.bind(this));
     }.bind(this);
 
     return promptForModuleName().then(handlePrompt);
   },
-  writing: function () {}
+  writing: function () {
+    // create new module
+    var newModule = this.props.moduleName;
+    var initType = this.props.initType;
+    var templateVars = {
+      moduleName: newModule,
+      moduleNameUp: newModule.charAt(0).toUpperCase() + newModule.slice(1),
+      typeLower: initType,
+      typeUpper: initType.charAt(0).toUpperCase() + initType.slice(1)
+    };
+    var moduleDir = 'src/' + newModule;
+    var moduleFilePath = moduleDir + '/' + newModule + '.module.js';
+    fs.mkdirSync(moduleDir);
+    this.fs.copyTpl(
+      this.templatePath('module.js'),
+      this.destinationPath(moduleFilePath),
+      templateVars
+    );
+    var factDest = 'src/' + newModule + '.' + initType + '.js';
+    this.fs.copyTpl(
+      this.templatePath('factory.js'),
+      this.destinationPath(factDest),
+      templateVars
+    );
+  }
 });
